8debda18faf3f45f798acb291433f9bff1b52efb
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-04-23 15:15:10 +0200

    working on day 2 tasks , Slide Show

--- Day2/Task/TasksResource/SlideShow/main.js
+++ Day2/Task/TasksResource/SlideShow/main.js
@@ -1,14 +1,27 @@
 var i = 1;
 var timerID;
 
-function changeImg() {
-    timerID = setTimeout(changeImg, 2000);
+function next() {
     if (i > 6) i = 1;
     document.images[0].src = i + ".jpg";
     i++;
 }
 
-function returnImg() {
+function previous() {
+    var j = i - 1;
+    if (j == 0) j = 6;
+    document.images[0].src = j + ".jpg";
+    i = j;
+}
+
+function SlideShow() {
+    timerID = setTimeout(SlideShow, 2000);
+    if (i > 6) i = 1;
+    document.images[0].src = i + ".jpg";
+    i++;
+}
+
+function stop() {
     clearTimeout(timerID);
-    document.images[0].src = "3.jpg";
+    document.images[0].src = i + ".jpg";
 }
