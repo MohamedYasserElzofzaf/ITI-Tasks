1b7bae481e89a20a0f39398f6a24fb7b9473f9b1
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-04-26 00:32:06 +0200

    working on day 4 &  add the c3 solution

--- Day 4/Function Object/c3.js
+++ Day 4/Function Object/c3.js
@@ -2,3 +2,19 @@
 // that takes any number of parameters
 // and
 // returns them reversed using array’s reverse function.
+
+function reversed(str) {
+    var arr = str.split("");
+    arr.reverse();
+    return console.log(arr.join(""));
+}
+var rev = function(str) {
+    var revers = "";
+    for (var character of str) {
+        revers = character + revers;
+    }
+    return console.log("second implementations " + revers);
+};
+
+reversed("Hello you!");
+rev("Hello world ");
