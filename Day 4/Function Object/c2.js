2b8405356ff941f0b5d8bb762ac6cf2729d927bb
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-04-25 23:42:25 +0200

    working on day 4 &  add the c2 solution

--- Day 4/Function Object/c2.js
+++ Day 4/Function Object/c2.js
@@ -2,3 +2,20 @@
 // Throw exception if the user passed any data type other than “number”
 // or
 // called your function without passing any parameters.
+function adding() {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        var num1 = arguments[i];
+        if (isNaN(num1)) {
+            throw "You passed data type other than NUMBER";
+        } else {
+            sum += arguments[i];
+        }
+    }
+    return console.log("you enter all numbers and their sum is : " + sum);
+}
+try {
+    adding(1, 3, 4, 5, "hello");
+} catch (e) {
+    console.error(e);
+}
