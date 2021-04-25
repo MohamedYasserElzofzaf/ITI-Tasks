67df4c7a8f244fadb66f220f61894afb5c94cb89
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-04-26 00:13:02 +0200

    working on day 4 &  add the c2 solution

--- Day 4/Function Object/c2.js
+++ Day 4/Function Object/c2.js
@@ -4,8 +4,12 @@
 // called your function without passing any parameters.
 function adding() {
     var sum = 0;
+
     for (var i = 0; i < arguments.length; i++) {
         var num1 = arguments[i];
+        if (!arguments.length) {
+            throw "You passed NO parameters";
+        }
         if (isNaN(num1)) {
             throw "You passed data type other than NUMBER";
         } else {
@@ -15,7 +19,7 @@ function adding() {
     return console.log("you enter all numbers and their sum is : " + sum);
 }
 try {
-    adding(1, 3, 4, 5, "hello");
+    adding(4, 3, 2);
 } catch (e) {
     console.error(e);
 }
