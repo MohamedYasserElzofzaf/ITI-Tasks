32e45ff77a6888f5a91febb30c054d36b8e41d53
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-04-27 01:18:15 +0200

    working on day 3 && cookies

--- Day 3/cookies.js
+++ Day 3/cookies.js
@@ -16,3 +16,58 @@
 // o returns a list of all stored cookies
 //  hasCookie(cookieName):
 // o Check whether a cookie exists or not
+
+function saveInfo() {
+    var date = new Date();
+    date.setMonth(date.getMonth() + 2);
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    // var male = document.getElementById("male").value;
+    // var female = document.getElementById("female").value;
+    // var favcolor = document.getElementById("favcolor").value;
+
+    document.cookie = "usernamecookie=" + name + ";expires=" + date;
+    document.cookie = "useragecookie= " + age + ";expires=" + date;
+    // document.cookie = "UserGenderCookie" +  + ";expires=" + date;
+    // document.cookie;
+}
+
+function getcookie() {
+    var assoiativeCookie = [];
+    var splitCookie = document.cookie.split(";");
+    for (var i = 0; i < splitCookie.length; i++) {
+        assoiativeCookie[splitCookie[i].split("=")[0].trim()] = splitCookie[
+            i
+        ].split("=")[1];
+    }
+    return assoiativeCookie;
+}
+
+// function displayInfo() {
+//     var cookieVals = document.cookie;
+//     var firstcookieIdx = cookieVals.indexOf("usernamecookie");
+//     var firstcookieLstIdx = cookieVals.indexOf(";", firstcookieIdx);
+
+//     var usrNameCookie = cookieVals.substring(firstcookieIdx, firstcookieLstIdx);
+//     var usrNameCookieName = usrNameCookie.split("=")[0];
+//     var usrNameCookieval = usrNameCookie.split("=")[1];
+
+//     var secondCookieIdx = cookieVals.indexOf("useragecookie");
+//     var secondCookieLstIdx = cookieVals.indexOf(";", secondCookieIdx);
+//     if (secondCookieLstIdx < 0) {
+//         var secondCookie = cookieVals.substring(secondCookieIdx, cookieVals.length);
+//     } else {
+//         var secondCookie = cookieVals.substring(
+//             secondCookieIdx,
+//             secondCookieLstIdx
+//         );
+//     }
+//     var ageCookieName = secondCookie.split("=")[0];
+//     var ageCookieval = secondCookie.split("=")[1];
+
+//     document.getElementById("div1").innerHTML =
+//         "Welcome " +
+//         usrNameCookieval +
+//         " You have visited this site " +
+//         ageCookieval;
+// }
