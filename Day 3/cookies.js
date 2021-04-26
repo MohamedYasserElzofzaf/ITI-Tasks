bc2fa13118f6445bd9333ae15017b1c5c7a4802f
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-04-26 12:57:14 +0200

    working on day 4 & modify the name of folders

---
+++ Day 3/cookies.js
@@ -0,0 +1,18 @@
+// Make your own .js library to create,
+// display and delete cookies,
+// then use it in creating the required cookies to display a greeting message to your visitor
+// with displaying an image as his profile pic referring to his gender,
+// and inform him with his number of visits to the site.
+// Display user name and number of visits with font color according to his choices.
+// Replace the registration page with the profile page using location object
+// the library should have the following functions:
+//  getCookie(cookieName):
+// o Retrieves a cookie value based on a cookie name.
+//  setCookie(cookieName,cookieValue[,expiryDate]):
+// o Sets a cookie based on a cookie name, cookie value, and expiration date.
+//  deleteCookie(cookieName):
+// o Deletes a cookie based on a cookie name.
+//  allCookieList():
+// o returns a list of all stored cookies
+//  hasCookie(cookieName):
+// o Check whether a cookie exists or not
--- Day 4/Function Object/c4.js
+++ Day 4/Function Object/c4.js
@@ -3,3 +3,4 @@
 // This object may have description property of string value if needed
 // Let any other created object can use this function property to generate getters and setters for his own properties
 // Avoid generating getters or setters for property of function value
+
