cf6dedd46cdab1e6c5c0aaf07159adcb0ee9e155
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-04-27 03:30:02 +0200

    working on day 3 && Event object

--- Day 3/Event object/B2.js
+++ Day 3/Event object/B2.js
@@ -1,2 +1,24 @@
 // B.2. Prevent context menu from being displayed by preventing the mouse right click from its action.
 // Note: context menu is the menu displayed when mouse right button is clicked
+
+var prev = document.getElementById("prevent");
+
+function handler(event) {
+    event = event || window.event;
+
+    if (event.stopPropagation) event.stopPropagation();
+
+    event.cancelBubble = true;
+    return false;
+}
+
+prev.innerHTML = "right-click here";
+
+prev.onclick = handler;
+prev.onmousedown = handler;
+prev.onmouseup = handler;
+
+////////////////////////////////////
+[...document.querySelectorAll(".prevCon")].forEach((el) =>
+    el.addEventListener("contextmenu", (e) => e.preventDefault())
+);
