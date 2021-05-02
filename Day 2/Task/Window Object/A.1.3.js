c93ef5731369001da7e6d69fca855a3250903e05
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-02 19:36:25 +0200

    working on missed assignmentns || assignment 2 task 1.3

--- Day 2/Task/Window Object/A.1.3.js
+++ Day 2/Task/Window Object/A.1.3.js
@@ -1,1 +1,8 @@
-// A.1.3. Create a parent a window that opens a scrollable advertising child window. (Bonus)
+// A.1.3. Create a parent a window that opens a scrollable advertis
+
+function ad() {
+    var mywin = open("Ad.html", " ", "width=400,height=300");
+    setInterval(function() {
+        mywin.scrollBy(15, 15);
+    }, 500);
+}
