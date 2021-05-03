af320d284037e2cb4a6a444605be636ba0904ac9
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-03 15:33:42 +0200

    working on missed assignmentns || assignment 2 task calculator

--- Day 2/Task/TasksResource/Calculator/main.js
+++ Day 2/Task/TasksResource/Calculator/main.js
@@ -1,15 +1,27 @@
+var result;
+var operator;
+
 function EnterNumber(val) {
-    document.getElementById("ans").value += val;
+    result = document.getElementById("Answer").value;
+    if (result.length < 20) {
+        document.getElementById("Answer").value += val;
+    }
 }
 
 function EnterEqual() {
-    var x = document.getElementById("ans").value;
-    var y = eval(x);
-    document.getElementById("ans").value = y;
+    // var x = document.getElementById("Answer").value;
+    // var y = eval(x);
+    // document.getElementById("Answer").value = y;
+    var sum = document.getElementById("Answer").value;
+    if (sum) {
+        document.getElementById("Answer").value = eval(sum);
+    }
 }
 
-function EnterOperator() {}
+function EnterOperator(val) {
+    operator = document.getElementById("Answer").value += val;
+}
 
 function EnterClear() {
-    document.getElementById("ans").value = "";
+    document.getElementById("Answer").value = "";
 }
