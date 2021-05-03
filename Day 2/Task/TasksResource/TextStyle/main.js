97b90ace82f8839c0fb0bb3541564bebdcf6582f
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-03 19:19:52 +0200

    working on missed assignmentns || assignment 2 task text style

---
+++ Day 2/Task/TasksResource/TextStyle/main.js
@@ -0,0 +1,37 @@
+var style;
+
+function ChangeFont(x) {
+    style = document.getElementById("PAR").style.fontFamily = x;
+}
+
+function ChangeAlign(x) {
+    style = document.getElementById("PAR").style.textAlign = x;
+}
+
+function ChangeHeight(x) {
+    style = document.getElementById("PAR").style.lineHeight = x;
+}
+
+function ChangeLSpace(x) {
+    style = document.getElementById("PAR").style.letterSpacing = x;
+}
+
+function ChangeIndent(x) {
+    style = document.getElementById("PAR").style.textIndent = x;
+}
+
+function ChangeTransform(x) {
+    style = document.getElementById("PAR").style.textTransform = x;
+}
+
+function ChangeDecorate(x) {
+    style = document.getElementById("PAR").style.textDecoration = x;
+}
+
+function ChangeBorder(x) {
+    style = document.getElementById("PAR").style.borderStyle = x;
+}
+
+function ChangeBorderColor(x) {
+    style = document.getElementById("PAR").style.borderColor = x;
+}
