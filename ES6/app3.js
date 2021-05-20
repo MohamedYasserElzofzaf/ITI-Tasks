733f824ee8f112d87c96439d44f0e8decd130a33
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-21 00:41:53 +0200

    Working with ES6 Task

--- ES6/app3.js
+++ ES6/app3.js
@@ -3,3 +3,13 @@
     so that if any long string (e.g. its length exceed 15 characters )called replace and 
     pass your object as replace method parameter it will display only 15 character of string with terminating “…”
 */
+
+let object = {
+    [Symbol.replace](str) {
+        if (str.length > 15) {
+            let newWord = str.slice(15);
+            return ...newWord;
+        }
+    },
+};
+console.log("HelloDarknessMyOldFreind!!!!!!!!!!!!!!!!!!!!!!".replace(object));
