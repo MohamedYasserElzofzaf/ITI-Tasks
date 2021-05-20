e53cd25947a60b5ba7c2450ab5a0fd5917665249
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-21 00:25:50 +0200

    Working with ES6 Task

---
+++ ES6/app3.js
@@ -0,0 +1,5 @@
+/*
+    3) create your own object that has [Symbol.replace] method 
+    so that if any long string (e.g. its length exceed 15 characters )called replace and 
+    pass your object as replace method parameter it will display only 15 character of string with terminating “…”
+*/
