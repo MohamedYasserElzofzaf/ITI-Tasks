ba54209e964b440d09ae55d44fdad4007c7a0650
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-21 23:26:27 +0200

    Working with ES6 task

---
+++ ES6/ajax.js
@@ -0,0 +1,41 @@
+function fetch() {
+    fetch("std.json")
+        .then(validateResponse)
+        .then(readResponse)
+        .then(logResult)
+        .catch(logError);
+}
+
+function validateResponse(response) {
+    if (!response.ok) throw Error(response.statusText);
+    return response;
+}
+
+function readResponse(response) {
+    return response.json();
+}
+
+function logResult(data) {
+    createTable(data);
+}
+
+function logError(data) {
+    console.error(data);
+}
+
+function createTable(data) {
+    var tableBody = document.getElementById("tContent");
+    for (var i of data) {
+        var x = i;
+        var row = document.createElement("tr");
+        row.innerHTML =
+            "<td>" +
+            x["nm"] +
+            "</td><td>" +
+            x["id"] +
+            "</td><td>" +
+            x["joinDate"] +
+            "</td>";
+        tableBody.appendChild(row);
+    }
+}
