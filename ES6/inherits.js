a5551e41a632730636667842cdaeafd6572a68e6
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-24 22:14:07 +0200

    Working with ES6 task

---
+++ ES6/inherits.js
@@ -0,0 +1,11 @@
+class polygon {
+    width = 4;
+    height = 5;
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Area() {
+        return this.width * this.height;
+    }
+}
