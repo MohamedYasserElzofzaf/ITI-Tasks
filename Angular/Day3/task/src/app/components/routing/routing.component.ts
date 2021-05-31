8d753d8c104f090493f855f5797d3129d26c1a3a
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-31 13:15:33 +0200

    Working on routring in angular

---
+++ Angular/Day3/task/src/app/components/routing/routing.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-routing',
+  templateUrl: './routing.component.html',
+  styles: [
+  ]
+})
+export class RoutingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
