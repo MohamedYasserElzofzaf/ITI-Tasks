c4b355ed85279aeba9b7a6f758a18ae56b2b27e3
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-04 11:21:35 +0200

    working on Angular Task

--- Angular/Day4/validation/src/app/app.component.spec.ts
+++ Angular/Day4/validation/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projectFour'`, () => {
+  it(`should have as title 'projectThree'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projectFour');
+    expect(app.title).toEqual('projectThree');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('projectFour app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('projectThree app is running!');
   });
 });
