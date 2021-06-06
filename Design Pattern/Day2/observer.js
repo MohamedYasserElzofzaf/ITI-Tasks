b0a518b22e957283b57bd46beedaaa79d39622c3
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-06 22:15:26 +0200

    Begin with Design Pattern task 1

---
+++ Design Pattern/Day2/observer.js
@@ -0,0 +1,19 @@
+/* With Using Observer design pattern
+Create the main functionality for Facebook (Post,Comments,Message,createAccount,Friend,Unfriend )
+a- create main class for Facebook accounts
+b- create IFaceBookSetting make it as Abstract class(Interface)
+contain the main members
+a- Array of Friends
+b- AddFreindFunction
+c- RemoveFriend
+d- sendMessageToAllFriend
+e- PostsPushNotification
+f- CommenstPushnotification
+g- SendMessageForPrivateFriend(Bouns)
+c- make Facebook Account implement the main functionality IFaceBookSetting
+d- create many Accounts (MyAccount ,Acc1,Acc2,Acc3,Acc4)
+e- Add (Acc1,Acc2,Acc3) as Friends for MyAccount
+f- Add new Post from My Account and fire notifications for all friends List
+g- Acc2 make new Post with Comment From MyAccount (Send Notify for Acc2 with MyAccountComment)
+h- Send Private Messages From MyAccount To Acc1 (Bouns)
+*/
