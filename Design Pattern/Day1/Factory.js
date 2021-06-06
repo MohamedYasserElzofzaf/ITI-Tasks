280e74cf714d9c4a66b03f2701c303dec7247737
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-06 16:46:23 +0200

    Begin with Design Pattern task 1

--- Design Pattern/Day1/Factory.js
+++ Design Pattern/Day1/Factory.js
@@ -5,3 +5,27 @@
     -make sure each time has only one Team lead
     --separate each type in different files
 */
+
+function Developer(_name, _age, _salary) {
+    this.name = _name;
+    this.age = _age;
+    this.salary = _salary;
+}
+
+function Tester(_name, _age, _salary) {
+    this.name = _name;
+    this.age = _age;
+    this.salary = _salary;
+}
+
+function QualityCon(_name, _age, _salary) {
+    this.name = _name;
+    this.age = _age;
+    this.salary = _salary;
+}
+
+function Designers(_name, _age, _salary) {
+    this.name = _name;
+    this.age = _age;
+    this.salary = _salary;
+}
