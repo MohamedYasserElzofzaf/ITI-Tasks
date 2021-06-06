7ae9ad969b88bb34b1214f4bc6dc55c48667a901
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-06 16:24:59 +0200

    Begin with Design Pattern task 1

---
+++ Design Pattern/Day1/Factory.js
@@ -0,0 +1,7 @@
+/*
+    2- Create your Company structure from employees with different departments 
+    (developing, Testing ,Quality controls and Designers)
+    Using factory DP create Array OF Employees and display their data
+    -make sure each time has only one Team lead
+    --separate each type in different files
+*/
