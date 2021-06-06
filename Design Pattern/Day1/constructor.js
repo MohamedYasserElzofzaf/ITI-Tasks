0afafd1c6ce3c5bf51e66f65ad932982f7e88f87
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-06 14:01:47 +0200

    Begin with Design Pattern task 1

---
+++ Design Pattern/Day1/constructor.js
@@ -0,0 +1,11 @@
+/* 
+    1- Using constructor DP Display the area and perimeter of an object
+    created from using Rectangle Constructor 
+    that has width and height properties and 2 extended methods calculating area, perimeter. 
+    Override .toString() function to display a message declaring the width, height, area and perimeter of the created object. 
+    Create Class Property that counts numbers of created objects and Class method to retrieve it.
+*/
+
+class Rectangle {
+    constructor() {}
+}
