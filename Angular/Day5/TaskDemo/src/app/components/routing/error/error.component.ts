963b8d6f7b5656f2e6a56c819162aff39befe81c
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-09 18:03:06 +0200

    working on Angular Task

--- Angular/Day5/TaskDemo/src/app/components/routing/error/error.component.ts
+++ Angular/Day5/TaskDemo/src/app/components/routing/error/error.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+  selector: 'app-error',
+  templateUrl: './error.component.html',
   styles: [
   ]
 })
-export class HomeComponent implements OnInit {
+export class ErrorComponent implements OnInit {
 
   constructor() { }
 
--- Angular/Day5/TaskDemo/src/app/components/routing/students/students.component.ts
+++ Angular/Day5/TaskDemo/src/app/components/routing/students/students.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
+  selector: 'app-students',
+  templateUrl: './students.component.html',
   styles: [
   ]
 })
-export class ProfileComponent implements OnInit {
+export class StudentsComponent implements OnInit {
 
   constructor() { }
 
