f8d807ac52d2f9cf7afb6407a33bd6a67f2df4ec
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-09 18:38:39 +0200

    working on Angular Task

--- Angular/Day5/TaskDemo/src/app/app.module.ts
+++ Angular/Day5/TaskDemo/src/app/app.module.ts
@@ -4,17 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/Ref&Componentinteraction/register/register.component';
 import { StudentComponent } from './components/Ref&Componentinteraction/student/student.component';
+import { StudentsComponent } from './components/routing/students/students.component';
+import { StudentDetailsComponent } from './components/routing/student-details/student-details.component';
+import { AboutComponent } from './components/routing/about/about.component';
+import { ErrorComponent } from './components/routing/error/error.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: StudentsComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'students/:id', component: StudentDetailsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: ErrorComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    StudentComponent
+    StudentComponent,
+    StudentsComponent,
+    StudentDetailsComponent,
+    AboutComponent,
+    ErrorComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
