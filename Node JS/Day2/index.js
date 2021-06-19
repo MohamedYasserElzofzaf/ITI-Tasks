6ebd846bfd137044991629aa1535cb405fb3cb3a
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-19 20:48:29 +0200

    working with node day 2 => simple server list directory content

--- Node JS/Day2/index.js
+++ Node JS/Day2/index.js
@@ -9,57 +9,73 @@ fs.readdir("./resources")
         throw err;
     });
 const server = http.createServer((req, res) => {
-            switch (req.url) {
-                case "/":
-                    {
-                        res.writeHead(200, {
-                            "content-type": "text/html",
-                        });
-                        home.map((file) => {
-                            res.write(`<li><a href="${file}">${file}</a></li><br>`);
-                        });
-                        res.end();
-                        break;
-                    }
-                    /* case "/index.html":
+    switch (req.url) {
+        case "/":
             {
                 res.writeHead(200, {
                     "content-type": "text/html",
                 });
-                fs.readFile(`./resources/index.html`).then((file) => {
-                    res.write(file);
-                    res.end();
+                home.map((file) => {
+                    res.write(`<li><a href="${file}">${file}</a></li><br>`);
                 });
-                break;
-            }
-        case "/image":
-            {
-                res.writeHead(200, {
-                    "content-type": "image/jpg",
-                });
-                fs.readFile(`./resources/ted.jpg`).then((image) => {
-                    res.write(image);
-                    res.end();
-                });
-                break;
-            }
-        case "/text":
-            {
-                res.writeHead(200, {
-                    "content-type": "text/txt",
-                });
-                fs.readFile(`./resources/text.txt`).then((file) => {
-                    res.write(file);
-                    res.end();
-                });
+                res.end();
                 break;
             }
         default:
-            {
-                res.statusCode = 404;
-                res.end();
-            }
-    }*/
-            }); server.listen(8000, () => {
-            console.log("server is listening");
-        });
+            home.find((url) => {
+                res.writeHead(200, { "content-type": "text/html" });
+                if (`/${url}` == req.url) {
+                    fs.readFile(`./resources/${url}`)
+                        .then((file) => {
+                            res.write(file);
+                            res.end();
+                        })
+                        .catch((err) => {
+                            throw err;
+                        });
+                }
+            });
+            break;
+    }
+    /* case "/index":
+          {
+              res.writeHead(200, {
+                  "content-type": "text/html",
+              });
+              fs.readFile(`./resources/index.html`).then((file) => {
+                  res.write(file);
+                  res.end();
+              });
+              break;
+          }
+      case "/image":
+          {
+              res.writeHead(200, {
+                  "content-type": "image/jpg",
+              });
+              fs.readFile(`./resources/ted.jpg`).then((image) => {
+                  res.write(image);
+                  res.end();
+              });
+              break;
+          }
+      case "/text":
+          {
+              res.writeHead(200, {
+                  "content-type": "text/txt",
+              });
+              fs.readFile(`./resources/text.txt`).then((file) => {
+                  res.write(file);
+                  res.end();
+              });
+              break;
+          }
+      default:
+          {
+              res.statusCode = 404;
+              res.end();
+          }*/
+});
+server.listen(8000, () => {
+    console.log("server is listening");
+});
