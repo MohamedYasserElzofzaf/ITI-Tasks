b11f2a35ea5b0c26b467bb43de0ea9dea517031f
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-24 00:02:58 +0200

    React project for task 1 & making the image slider

--- React/task1/src/components/slider.js
+++ React/task1/src/components/slider.js
@@ -1,26 +1,42 @@
+import { render } from "@testing-library/react";
 import React from "react";
 
 class imageSlider extends React.Component {
+    imgsURL = [
+        "/Ted/2.jpg",
+        "/Ted/3.jpg",
+        "/Ted/4.jpg",
+        "/Ted/5.jpg",
+        "/Ted/6.jpg",
+        "/Ted/7.jpg",
+        "/Ted/8.jpg",
+        "/Ted/9.jpg",
+        "/Ted/10.jpg",
+        "/Ted/007.jpg",
+    ];
     constructor() {
         super();
         this.state = {
-            imgURL: "/Ted/1.jpg",
             i: 0,
+            imgURL: this.imgsURL[0],
         };
     }
     next = () => {
         let { i } = this.state;
-        if (i < 10) {
-            this.setState({
-                i: i++,
-                imgURL: "/Ted/" + this.i + ".jpg",
-            });
-            console.log("clicked");
-        }
+        if (this.imgsURL.length - 1 > i) i++;
+        let img = this.imgsURL[i];
+        this.setState({ i, imgURL: img });
+        // console.log("clicked");
     };
-    prev = () => {};
-    play = () => {};
-    stop = () => {};
+
+    prev = () => {
+        let { i } = this.state;
+        if (i > 0) i--;
+        let img = this.imgsURL[i];
+        this.setState({ i, imgURL: img });
+    };
+    // play = () => {}
+    // stop = () => {}
     render() {
         const styles = {
             container: {
@@ -32,10 +48,20 @@ class imageSlider extends React.Component {
                 textAlign: "center",
             },
             img: {
-                border: "1 px solid# ddd",
-                borderRadius: "4 px",
-                padding: "5 px",
-                width: "150 px",
+                border: "1px solid",
+                borderRadius: "10px",
+                margin: "auto",
+                width: "500px",
+                height: "300px",
+                marginBottom: "20px",
+            },
+            btn: {
+                backgroundColor: "#008CBA",
+                fontSize: "12px",
+                padding: "12px 28px",
+                borderRadius: "10px",
+                boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                float: "left",
             },
         };
         return ( <
@@ -47,11 +73,18 @@ class imageSlider extends React.Component {
             alt = "TedLasso" / >
             <
             input type = "button"
+            style = { styles.btn }
             value = "Next"
             onClick = { this.next }
+            />{" "} <
+            input type = "button"
+            style = { styles.btn }
+            value = "Previous"
+            onClick = { this.prev }
             />{" "} <
             /div>
         );
     }
 }
+
 export default imageSlider;
