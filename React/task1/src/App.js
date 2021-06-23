b9c0bf85e2d777ce1a8335c8730abe47f2d55165
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-23 14:31:49 +0200

    React project for task 1 & making the image slider

--- React/task1/src/App.js
+++ React/task1/src/App.js
@@ -1,12 +1,15 @@
 import { func } from "prop-types";
 import React from "react";
 import Text from "./components/TextChange";
+import imageSlider from "./components/slider";
 
 function App() {
     return ( <
         div className = "App" >
         <
         Text / >
+        <
+        imageSlider / >
         <
         /div>
     );
---
+++ React/task1/src/components/slider.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+class imageSlider extends React.Component {}
+export default imageSlider;
