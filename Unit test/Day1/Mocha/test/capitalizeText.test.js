3db27702052644fbac712a15635a3721b6f80a3f
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-30 15:50:09 +0200

    working on mocha

--- Unit test/Day1/Mocha/test/capitalizeText.test.js
+++ Unit test/Day1/Mocha/test/capitalizeText.test.js
@@ -1,22 +1,13 @@
-// const { capitalizeText } = require("../index");
-// const expect = require("chai").expect;
-// const assert = require("chai").assert;
-// const should = require("chai").should();
-
 describe("Test capitalText function return capitalized string", () => {
-  it("should output capitalized string ", () => {
-    capitalizeText("hamada").should.equal("HAMADA");
-    // expect(capitalizeText("hamada")).to.equal("SOMETEXT");
-    // assert.equal(capitalizeText("hamada"), "SOMETEXT");
-  });
+    it("should output capitalized string ", () => {
+        capitalizeText("yasser").should.equal("YASSER");
+    });
 
-  it("should output string type", () => {
-    capitalizeText("hamada").should.be.a("string");
-    // expect(capitalizeText("someText")).to.be.a("string");
-    // assert.isString(capitalizeText("someText"));
-  });
+    it("should output string type", () => {
+        capitalizeText("yasser").should.be.a("string");
+    });
 
-  it("should throw error", () => {
-    expect(capitalizeText).to.throw(TypeError, "parameter should be string");
-  });
+    it("should throw error", () => {
+        expect(capitalizeText).to.throw(TypeError, "parameter should be string");
+    });
 });
--- Unit test/Day1/Mocha/test/createArray.test.js
+++ Unit test/Day1/Mocha/test/createArray.test.js
@@ -1,24 +1,20 @@
-// const { createArray } = require("../index");
-// const expect = require("chai").expect;
-// const assert = require("chai").assert;
-
 describe("Test createArray function", () => {
-  let length = 0;
-  beforeEach(() => {
-    length++;
-  });
+    let length = 0;
+    beforeEach(() => {
+        length++;
+    });
 
-  it("should be return lenght of array", () => {
-    expect(createArray(3)).to.have.lengthOf(3);
-  });
+    it("should be return length of array", () => {
+        expect(createArray(3)).to.have.lengthOf(3);
+    });
 
-  it("should be return array type", () => {
-    expect(createArray(3)).to.be.an("array");
-  });
+    it("should be return array type", () => {
+        expect(createArray(3)).to.be.an("array");
+    });
 
-  it("should be return length of length variable", () => {
-    assert.lengthOf(createArray(length), length, "Not Equal");
-  });
+    it("should be return length of length variable", () => {
+        assert.lengthOf(createArray(length), length, "Not Equal");
+    });
 
-  it("pending test");
+    it("pending test");
 });
