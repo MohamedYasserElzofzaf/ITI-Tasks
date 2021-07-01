3481c4c7a99b15ac38d0676f39617cf68e20a272
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-07-01 19:49:08 +0200

    working on styling

--- React/task2/src/components/notFound.js
+++ React/task2/src/components/notFound.js
@@ -3,24 +3,18 @@ import React from "react";
 class NotFound extends React.Component {
     constructor() {
         super();
-        this.state = {
-            imge: "./imges/404.jpg",
-        };
     }
 
     render() {
         return ( <
             div className = "container" >
             <
-            img className = "imge"
-            alt = "not-found"
-            src = { this.state.imge }
-            /> <
+            h1 > Page Not found < /h1>{" "} <
             button className = "btn btn-primary"
             onClick = {
                 () => this.props.history.replace("/") } >
-            Back To Home <
-            /button> <
+            Back To Home { " " } <
+            /button>{" "} <
             /div>
         );
     }
